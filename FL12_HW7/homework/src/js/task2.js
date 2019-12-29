0e728c0f20e574d3d9285a969e444ad51aa328ff
karina-shribaylo <karynamishchenko@gmail.com>
2019-12-29 22:32:56 +0200

    'HW8'

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -4,12 +4,14 @@ confirm('Do you want to play a game?') ? game = true : alert('You did not become
 const three = 3;
 const eight = 8;
 const hundred = 100;
+const rangeIncrement = 4;
+const prizeIncrement = 2;
+
+
 let maxRandomNumber = 8;
 let firstAttemptPrize = 100;
 let secondAttemptPrize = 50;
 let thirdAttemptPrize = 25;
-const rangeIncrement = 4;
-const prizeIncrement = 2;
 let prize = 0;
 let userBet;
 let attempts = 3;
@@ -23,7 +25,7 @@ while (game) {
     let prizeValues = [thirdAttemptPrize, secondAttemptPrize, firstAttemptPrize];
     let pocketNumber = Math.floor(Math.random() * (maxRandomNumber + 1));
 
-    // one level of the game with 3 attempts
+
     while (attempts) {
         userBet = parseInt(prompt(`Enter a number from 0 to ${maxRandomNumber}
 Attempts left: ${attempts--}
@@ -46,7 +48,7 @@ Possible prize on current attempt: $${prizeValues[attempts]}`));
         }
     }
 
-    // ask user to start over if they lost (used all 3 attempts) or decided not to continue after win
+
     if (gameOver) {
         alert(`Thank you for your participation. Your prize is: $${prize}`);
         let anotherAsk = confirm('Do you wat to play again ?');
